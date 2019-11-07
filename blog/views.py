2946daf1559feee214fb6fa42fc7d5445eaddf41
雨过初晴 <admin@choyeon.cn>
2019-11-07 16:22:53 +0800

    测试模版

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request, category_id=None, tag_id=None):
-    content = 'post_list category_id={category_id},tag_id={tag_id}'.format(category_id=category_id, tag_id=tag_id)
-    return HttpResponse(content)
+    return render(request, 'blog/list.html', content={'name': 'post_list'})
 
 
 def post_detail(request, post_id):
-    return HttpResponse('detail')
+    return render(request, 'blog/detail.html', content={'name': 'post_detail'})
